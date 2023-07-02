26a715d841533da9e86f9d0fcc9567c82ba6508b
kevin smulders <39293162+kevinsmul@users.noreply.github.com>
2023-07-02 20:14:04 +0200

    relocate currency and fix alive count bug

--- Assets/Code/Scripts/Health.cs
+++ Assets/Code/Scripts/Health.cs
@@ -6,11 +6,16 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoint = 2;
+    [SerializeField] private int currencyWorth = 50;
+
+    private bool isDestroyed = false;
 
     public void TakeDamage(int dmg){
         hitPoint -= dmg;
-        if(hitPoint <= 0){
+        if(hitPoint <= 0 && !isDestroyed){
+            LevelManager.main.IncreaseCurrency(currencyWorth);
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
