0182509828dbde69a12a4506aee8339f8c5b965d
Simchaja <53301183+Simchaja@users.noreply.github.com>
2023-07-02 15:58:07 +0200

    Turrets

---
+++ Assets/Turret.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private Transform target;
+
+    private void onDrawGizmosSelected() {
+
+        Handles.color = Color.cyan; 
+        Handles.DrawWireDisc(transform.position,transform.forward, targetingRange);
+    }
+
+    // Start is called before the first frame update
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if(target == null){
+            findTarget();
+            return;
+        }
+
+        rotateTowardsTarget();
+
+        if(!CheckTargetIsInRange()) {
+            target = null;
+        } 
+    }
+
+    private void findTarget(){
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2) transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0){
+            target = hits[0].transform;
+        }
+        {
+            
+        }
+    }
+
+    private void rotateTowardsTarget() {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg  + 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckTargetIsInRange(){
+        return Vector2.Distance(target.position, transform.position) <= targetingRange; 
+    }
+}
