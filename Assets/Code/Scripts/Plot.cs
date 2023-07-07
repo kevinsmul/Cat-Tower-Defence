7df319ee08e740c9946ae6e439040f91b867a9c0
kevin smulders <39293162+kevinsmul@users.noreply.github.com>
2023-07-07 20:42:49 +0200

    add the upgrade tower feature to the base tower

--- Assets/Code/Scripts/Plot.cs
+++ Assets/Code/Scripts/Plot.cs
@@ -8,7 +8,8 @@ public class Plot : MonoBehaviour
     [SerializeField] private SpriteRenderer sr;
     [SerializeField] private Color hoverColor;
 
-    private GameObject tower;
+    private GameObject towerObj;
+    public Turret turret;
     private Color startColor;
 
     private void Start(){
@@ -24,7 +25,12 @@ public class Plot : MonoBehaviour
     }
 
     private void OnMouseDown() {
-        if (tower != null) return;
+        if (UIManager.main.IsHoveringUI()) return;
+        
+        if (towerObj != null){
+            turret.OpenUpgradeUI();
+            return;
+        };
 
 
         Tower towerToBuild = BuildManager.main.getSelectedTower();
@@ -32,6 +38,7 @@ public class Plot : MonoBehaviour
             return;
         }
         LevelManager.main.SpendCurrency(towerToBuild.cost);
-        tower = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity);
+        towerObj = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity);
+        turret = towerObj.GetComponent<Turret>();
     }
 }
--- Assets/Code/Scripts/Turret.cs
+++ Assets/Code/Scripts/Turret.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 
 public class Turret : MonoBehaviour
@@ -10,25 +11,29 @@ public class Turret : MonoBehaviour
     [SerializeField] private LayerMask enemyMask;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firingPoint;
+    [SerializeField] private GameObject upgradeUI;
+    [SerializeField] private Button upgradeButton;
 
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private float rotationSpeed = 5f;
     [SerializeField] private float bps = 1f; // Bullet per second
+    [SerializeField] private int baseUpgradeCost = 100;
 
+    private float bpsBase;
+    private float targetingRangeBase;
 
     private Transform target;
     private float timeUntilFire;
 
-    private void onDrawGizmosSelected() {
-
-        Handles.color = Color.cyan; 
-        Handles.DrawWireDisc(transform.position,transform.forward, targetingRange);
-    }
+    private int level = 1;
 
     // Start is called before the first frame update
     private void Start() {
+        bpsBase = bps;
+        targetingRangeBase = targetingRange;
+        upgradeButton.onClick.AddListener(Upgrade);
     }
 
     // Update is called once per frame
@@ -75,4 +80,36 @@ public class Turret : MonoBehaviour
     private bool CheckTargetIsInRange(){
         return Vector2.Distance(target.position, transform.position) <= targetingRange; 
     }
+
+    public void OpenUpgradeUI(){ 
+        upgradeUI.SetActive(true);
+    }
+
+    public void CloseUpgradeUI(){ 
+        upgradeUI.SetActive(false);
+        UIManager.main.SetHoveringState(false);
+    }
+
+    public void Upgrade(){
+        if (CalculateCost() > LevelManager.main.currency) return;
+        LevelManager.main.SpendCurrency(CalculateCost());
+        level++;
+        bps = CalculateBPS();
+        targetingRange = calculateRange();
+        CloseUpgradeUI();
+    }
+
+    private int CalculateCost(){
+        return Mathf.RoundToInt(baseUpgradeCost * Mathf.Pow(level, 0.8f));
+    }
+
+    private float CalculateBPS(){
+        return bpsBase * Mathf.Pow(level, 0.6f);
+    }
+
+    private float calculateRange(){
+        return targetingRange * Mathf.Pow(level, 0.4f);
+    }
+
+
 }
